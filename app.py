5ba649f5176ca687ffbd8adc8a202feab522abb1
Barry Fung <barry.kl.fung@gmail.com>
2017-04-27 14:46:12 -0400

    typos

--- app.py
+++ app.py
@@ -180,4 +180,4 @@ def scrape_for_recipes():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=(port==5000))s
+    app.run(host='0.0.0.0', port=port, debug=(port==5000))
